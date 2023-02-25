0ef70c2ec03a184304645b84efc00e670312bb31
lucas brown <lucas20brown@hotmail.co.uk>
2023-02-25 19:29:07 +0000

    added styling to albums page, also took albums table out of schema

--- travel-journal/src/pages/components/Picture.jsx
+++ travel-journal/src/pages/components/Picture.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import stickyTape from "../../assets/sticky-tape.png"
 
 function Picture(props) {
-  const randomClass = Math.random() < 0.5 ? 'card--sticky_tape_1' : 'card--sticky_tape_2';
-  const cardRandomClass = randomClass === 'card--sticky_tape_1' ? "picture--img_2" : "picture--img_1"
+  const randomClass = Math.random() < 0.5 ? 'picture--sticky_tape_1' : 'picture--sticky_tape_2';
+  const pictureRandomClass = randomClass === 'picture--sticky_tape_1' ? "picture--img_2" : "picture--img_1"
 
   return (
     <div className="picture">
+      <img className={pictureRandomClass} src={props.url} alt="img" />
       <img className={randomClass} src={stickyTape} alt="sticky tape" />
-      <img className={cardRandomClass} src={props.url} alt="img" />
     </div>
   )
 }
